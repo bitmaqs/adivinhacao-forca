24ee70ae96f63984502bd326762b980017f333fd
Marques <bitmaqs@gmail.com>
2018-11-13 15:50:51 -0300

    forca.py

--- forca.py
+++ forca.py
@@ -1,10 +1,26 @@
 def jogar():
-    print("*************************************")
-    print("**** Bem vindo ao jogo de Forca! ****")
-    print("*************************************")
+    print('*****************************************')
+    print('****** Bem-vindo ao jogo da Forca *******')
+    print('*****************************************')
 
+    palavra_secreta = "banana"
+    enforcou = False
+    acertou = False
 
-    print("Fim de Jogo!")
+    while (not acertou and not enforcou):
 
-if  (__name__ == "__main__"):
+        chute = input("Qual letra? ")
+        chute = chute.strip()
+
+        index = 0
+        for letra in palavra_secreta:
+            if (chute.upper() == letra.upper()):
+                print("Encontrei a letra {} na posicao {}".format(letra, index))
+            index = index + 1
+
+        print("Jogando...")
+
+    print("Fim do jogo")
+
+if(__name__ == '__main__'):
     jogar()
