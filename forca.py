a4e69f040d48a188a4a39ecd83c2162095d09e31
Marques <bitmaqs@gmail.com>
2018-11-15 19:20:47 -0300

    lists

--- forca.py
+++ forca.py
@@ -1,11 +1,14 @@
 def jogar():
-    print('*****************************************')
-    print('****** Bem-vindo ao jogo da Forca *******')
-    print('*****************************************')
+    print("*****************************************")
+    print("****** Bem-vindo ao jogo da Forca *******")
+    print("*****************************************")
 
     palavra_secreta = "banana"
+    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
     enforcou = False
     acertou = False
+
+    print(letras_acertadas)
 
     while (not acertou and not enforcou):
 
@@ -15,8 +18,11 @@ def jogar():
         index = 0
         for letra in palavra_secreta:
             if (chute.upper() == letra.upper()):
-                print("Encontrei a letra {} na posicao {}".format(letra, index))
+                letras_acertadas[index] = letra
             index = index + 1
+        print(letras_acertadas)
+        letras_faltando = str(letras_acertadas.count("_"))
+        print("Ainda faltam {} letras".format(letras_faltando))
 
         print("Jogando...")
 
