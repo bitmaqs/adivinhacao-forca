0e092393814f36bdc9ae7d8d8b811ad21adeae60
Marques <bitmaqs@gmail.com>
2018-11-17 11:10:50 -0300

    Implementando o encerramento do jogo

--- forca.py
+++ forca.py
@@ -3,30 +3,53 @@ def jogar():
     print("****** Bem-vindo ao jogo da Forca *******")
     print("*****************************************")
 
-    palavra_secreta = "banana"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_"]
-    enforcou = False
-    acertou = False
+    palavra_secreta = "banana".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
+    #enforcou = False
+    #acertou = False
+    erros = 0
 
     print(letras_acertadas)
 
-    while (not acertou and not enforcou):
+    while (True):
 
         chute = input("Qual letra? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if (chute.upper() == letra.upper()):
-                letras_acertadas[index] = letra
-            index = index + 1
+        if (chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute.upper() == letra.upper()):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+            print("OOPS! A palavra não possui a letra {}\n{} Tentativas Restantes\n".format(chute, 6-erros))
+
+        if ("_" not in letras_acertadas):
+            print("\n**** {} ****\n".format(palavra_secreta))
+            print("Você ganhou!! :)\n")
+            break
+        elif (erros == 6):
+            print("Você perdeu!! :(\n")
+            break
+
+
+        #enforcou = erros == 6
+        #acertou = "_" not in letras_acertadas
         print(letras_acertadas)
+
         letras_faltando = str(letras_acertadas.count("_"))
-        print("Ainda faltam {} letras".format(letras_faltando))
+        print("Faltam {} letras".format(letras_faltando))
 
-        print("Jogando...")
+        print("\nJogando...")
 
+    '''if(acertou):
+        print("Você ganhou!! :)")
+    else:
+        print("Você perdeu!! :(")'''
     print("Fim do jogo")
 
-if(__name__ == '__main__'):
+
+if ( __name__ == '__main__' ):
     jogar()
